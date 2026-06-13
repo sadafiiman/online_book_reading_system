6c1126c9ddb8c2745bc7f35fadd8ca0ca37fa9be
sadafiiman <sadafiiman@gmail.com>
2026-06-13 23:35:22 +0330

    chore: add books config for font size and chars per page

---
+++ config/books.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    /* Default Font Size */
+    'default_font_size' => (int) env('DEFAULT_FONT_SIZE', 16),
+
+    /* Number of characters that fit on one page at the default font size */
+    'base_chars_per_page' => (int) env('BASE_CHARS_PER_PAGE', 2000),
+];
